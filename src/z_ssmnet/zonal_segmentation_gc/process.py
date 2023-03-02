48b662ebd56cfb242b3196cbed3ef4c082348bd1
Yuan Yuan <45285331+yuanyuan29@users.noreply.github.com>
2023-03-02 20:53:34 +1100

    Update process.py

--- src/z_ssmnet/zonal_segmentation_gc/process.py
+++ src/z_ssmnet/zonal_segmentation_gc/process.py
@@ -10,6 +10,7 @@ from picai_prep.data_utils import atomic_image_write
 from picai_prep.preprocessing import (PreprocessingSettings, Sample,
                                       resample_to_reference_scan)
 
+import numpy as np
 
 class MissingSequenceError(Exception):
     """Exception raised when a sequence is missing."""
@@ -31,10 +32,70 @@ def strip_metadata(img: sitk.Image) -> None:
     for key in img.GetMetaDataKeys():
         img.EraseMetaData(key)
 
+def mask_postprocessing(img: sitk.Image):
+    # calculate connected components
+    cc_filter = sitk.ConnectedComponentImageFilter()
+    cc_filter.SetFullyConnected(True)
+    out_mask = cc_filter.Execute(img)
+    out_mask_array = sitk.GetArrayFromImage(out_mask)
+    img_array = sitk.GetArrayFromImage(img)
 
-class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
+    # if there is only one component, we are done
+    if cc_filter.GetObjectCount() == 1:
+        return img
+
+    # if no, choose the largest component including the center point on y axis and set all others to zero
+    elif cc_filter.GetObjectCount() > 1:
+        cc_count = cc_filter.GetObjectCount()
+        cc_count_list = list(np.arange(1, cc_count+1))
+        # set the connected component only including same label 2 as 0
+        for i in range(1, cc_filter.GetObjectCount() + 1):
+            if img_array[out_mask_array == i].min() == 2:
+                out_mask_array[out_mask_array == i] = 0
+                cc_count -= 1
+                cc_count_list.remove(i)
+                      
+        # find the largest component
+        largest_component = np.argmax(
+            [np.sum(out_mask_array == i) for i in cc_count_list]
+        )
+        # the label of the largest component
+        largest_component_label = cc_count_list[largest_component]
+
+        center_a = img_array.shape[2] // 2
+        if img.GetSize()[0] >1000:
+            prop = list(np.unique(out_mask_array[:, :, center_a]))
+            prop.remove(0)
+            # choose the prop nearest to the center of the image
+            Prop_largest = np.argmax([np.sum(out_mask_array == i) for i in prop])
+            Prop_largest_label = prop[Prop_largest]
+
+            img_array[out_mask_array != Prop_largest_label] = 0
+            img_out = sitk.GetImageFromArray(img_array)
+            img_out.CopyInformation(img)
+            return img_out
+        
+        # check here
+        elif largest_component_label in out_mask_array[:, :, center_a]:
+            # set all other components to zero
+            img_array[out_mask_array != largest_component_label] = 0
+            img_out = sitk.GetImageFromArray(img_array)
+            img_out.CopyInformation(img)
+            return img_out
+    
+        else:
+            # only keep the largest component
+            img_array[out_mask_array != largest_component_label] = 0
+            img_out = sitk.GetImageFromArray(img_array)
+            img_out.CopyInformation(img)
+            return img_out
+    else:
+        print("no connection component found")
+        return img
+    
+class ProstateZonalSegmentationAlgorithm(SegmentationAlgorithm):
     """
-    Wrapper to deploy trained prostate segmentation nnU-Net model from
+    Wrapper to deploy trained prostate zonal segmentation nnU-Net model from
     https://github.com/yuanyuan29/Z-SSMNet/ as a grand-challenge.org algorithm.
     """
 
@@ -54,7 +115,7 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
             "/input/images/transverse-adc-prostate-mri",
         ]
         self.scan_paths = []
-        self.prostate_segmentation_path = Path("/output/images/prostate-zonal-segmentation/prostate_gland.mha")
+        self.prostate_zonal_segmentation_path = Path("/output/images/prostate-zonal-segmentation/prostate_zonal_mask.mha")
 
         # input / output paths for nnUNet
         self.nnunet_inp_dir = Path("/opt/algorithm/nnunet/input")
@@ -64,7 +125,7 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
         # ensure required folders exist
         self.nnunet_inp_dir.mkdir(exist_ok=True, parents=True)
         self.nnunet_out_dir.mkdir(exist_ok=True, parents=True)
-        self.prostate_segmentation_path.parent.mkdir(exist_ok=True, parents=True)
+        self.prostate_zonal_segmentation_path.parent.mkdir(exist_ok=True, parents=True)
 
         # input validation for multiple inputs
         scan_glob_format = "*.mha"
@@ -86,10 +147,10 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
                 sitk.ReadImage(str(path))
                 for path in self.scan_paths
             ],
-            settings=PreprocessingSettings(
-                physical_size=[81.0, 192.0, 192.0],
-                crop_only=True
-            )
+            # settings=PreprocessingSettings(
+            #     physical_size=[81.0, 192.0, 192.0],
+            #     crop_only=True
+            # )
         )
 
         # perform preprocessing
@@ -103,7 +164,7 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
     # Note: need to overwrite process because of flexible inputs, which requires custom data loading
     def process(self):
         """
-        Load bpMRI scans and segment the whole prostate gland
+        Load bpMRI scans and segment the PZ and TZ of prostate gland
         """
         # perform preprocessing
         self.preprocess_input()
@@ -117,9 +178,12 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
             store_probability_maps=False,
         )
 
-        # read binarized and postprocessed prediction
+        # read binarized prediction
         pred_path = str(self.nnunet_out_dir / "scan.nii.gz")
         pred: sitk.Image = sitk.ReadImage(pred_path)
+
+        # postprocess to remove the noisy labels
+        pred = mask_postprocessing(pred)
 
         # transform prediction to original space
         reference_scan = sitk.ReadImage(str(self.scan_paths[0]))
@@ -129,7 +193,7 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
         strip_metadata(pred)
 
         # save prediction to output folder
-        atomic_image_write(pred, str(self.prostate_segmentation_path))
+        atomic_image_write(pred, str(self.prostate_zonal_segmentation_path))
 
     def predict(self, task, trainer="nnUNetTrainerV2", network="3d_fullres",
                 checkpoint="model_final_checkpoint", folds="0,1,2,3,4", store_probability_maps=True,
@@ -174,4 +238,4 @@ class ProstateSegmentationAlgorithm(SegmentationAlgorithm):
 
 
 if __name__ == "__main__":
-    ProstateSegmentationAlgorithm().process()
+    ProstateZonalSegmentationAlgorithm().process()
