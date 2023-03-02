15eefb3290d32e0103d0ed903405691fdbb726a1
Yuan Yuan <45285331+yuanyuan29@users.noreply.github.com>
2023-03-02 20:52:54 +1100

    Update export.sh

--- src/z_ssmnet/zonal_segmentation_gc/export.sh
+++ src/z_ssmnet/zonal_segmentation_gc/export.sh
@@ -2,5 +2,5 @@
 
 ./build.sh
 
-docker save yuanyuan29/z_ssmnet_prostate_segmentation_processor:latest | gzip -c > yuanyuan29_z_ssmnet_prostate_segmentation_processor.tar.gz
+docker save z_ssmnet_prostate_zonal_segmentation_processor:latest | gzip -c > z_ssmnet_prostate_zonal_segmentation_processor.tar.gz
 
