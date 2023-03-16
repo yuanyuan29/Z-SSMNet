68e2dac18d1801ae7ffb135970c2806b8b32f192
Joeran Bosma <joeran@bosma.co>
2023-03-16 09:31:12 +0100

    Read git token in build script

--- src/z_ssmnet/z_nnmnet/training_docker/build.sh
+++ src/z_ssmnet/z_nnmnet/training_docker/build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# read gittoken from file?
+if [ -f "gittoken" ];
+then
+    gittoken=`cat gittoken`
+    echo "Using gittoken from file"
+else
+    echo "Enter your git token (or save to gittoken file): "
+    read gittoken
+fi
+
+read -p "Enter the branch of yuanyuan29/Z-SSMNet you want to build [main]: " branch
+branch=${branch:-main}
+
+echo "Building yuanyuan29/Z-SSMNet docker image from branch $branch"
+
 docker build . \
-  --tag yuanyuan29/z-ssmnet:1.0.0-customized-v1.0 \
-  --tag yuanyuan29/z-ssmnet:latest
+    --no-cache --build-arg gittoken=$gittoken --build-arg branch=$branch \
+    --tag yuanyuan29/z-ssmnet:1.0.0-customized-v1.0 \
+    --tag yuanyuan29/z-ssmnet:latest
