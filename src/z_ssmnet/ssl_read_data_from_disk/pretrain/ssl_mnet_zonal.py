abf1cc9e1b4155bf7f9461bc28662301af7300e1
Joeran Bosma <joeran@bosma.co>
2023-04-12 21:53:57 +0200

    Automatically resume model pretraining

--- src/z_ssmnet/ssl_read_data_from_disk/pretrain/ssl_mnet_zonal.py
+++ src/z_ssmnet/ssl_read_data_from_disk/pretrain/ssl_mnet_zonal.py
@@ -37,6 +37,9 @@ def pretrain(
         os.makedirs(logs_path)
 
     config = Config()
+    model_path = os.path.join(model_dir, config.exp_name+".model")
+    if os.path.exists(model_path):
+        config.weights = model_path
     config.display()
 
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -166,8 +169,8 @@ def pretrain(
                 'epoch':epoch + 1,
                 'state_dict' : model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()
-            },os.path.join(model_dir, config.exp_name+".model"))
-            print("Saving model ",os.path.join(model_dir, config.exp_name+".model"))
+            },model_path)
+            print("Saving model ",model_path)
 
         else:
             print("Validation loss does not decrease from {:.4f}, num_epoch_no_improvement {}".format(best_loss,num_epoch_no_improvement))
